2d9e177b4117518f89ff6055430d7cf05f98b2b9
Noé <noefaure@orange.fr>
2017-05-19 11:53:38 +0200

    Fermeture à la sourie possible

--- Bomberman 2/src/Menu.java
+++ Bomberman 2/src/Menu.java
@@ -9,122 +9,254 @@ public class Menu {
 		int Affichage_ok = 0;
 		boolean boucle = true;
 		
+		
 		while(boucle == true){
 			
+			//Gestion sourie
+			if(StdDraw.mouseX() >= 707 && StdDraw.mouseX() < 750 && StdDraw.mouseY() < 200 && StdDraw.mouseY() >= 153){
+				if (choix == 0 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 242, "Survol Resume.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+					
+				}
+				
+				if (choix == 1 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 299, "Survol Restart.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+				}
+				
+				if (choix == 2 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 397, "Survol Quit.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+				}
+				
+				if(StdDraw.isKeyPressed(40) && choix < 2){
+					choix = choix + 1;
+					Affichage_ok = 0;
+					
+				}
+				
+				if(StdDraw.isKeyPressed(38) && choix >= 1){
+					choix = choix - 1;
+					Affichage_ok = 0;
+					
+				}
+				
+				//RESUME PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 0){
+					boucle = false;
+					
+				}
+				
+				//RESTART PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 1){
+					
+				}
+				
+				//QUIT PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 2){
+					System.exit(1);
+					
+				}
+				StdDraw.pause(5);
+				
+				StdDraw.picture(728, 173, "Close.png");
+				
+				//QUIT MOUSE
+				if (StdDraw.mousePressed()){
+					boucle = false;
+				}
+			}
+			
+			
+			//LA SOURIE N'EST PAS SUR LA CROIX
+			else{
+				if (choix == 0 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 242, "Survol Resume.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+					
+				}
+				
+				if (choix == 1 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 299, "Survol Restart.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+				}
+				
+				if (choix == 2 && Affichage_ok == 0){
+					Plateau_1.Afficher_map(map,Joueur1,Joueur2);
+					
+					StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
+					StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
+					StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
+					StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
+					
+					//Timer
+					if (Time_seconde < 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute < 10){
+						StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde < 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
+					}
+					else if(Time_seconde >= 10 && Time_minute >= 10){
+						StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
+					}
+					
+					StdDraw.picture(500, 300, "Menu.png");
+					StdDraw.picture(500, 397, "Survol Quit.png");
+					Affichage_ok = 1;
+					StdDraw.pause(80);
+				}
+				
+				if(StdDraw.isKeyPressed(40) && choix < 2){
+					choix = choix + 1;
+					Affichage_ok = 0;
+					
+				}
+				
+				if(StdDraw.isKeyPressed(38) && choix >= 1){
+					choix = choix - 1;
+					Affichage_ok = 0;
+					
+				}
+				
+				//RESUME PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 0){
+					boucle = false;
+					
+				}
+				
+				//RESTART PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 1){
+					
+				}
+				
+				//QUIT PRESS
+				if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 2){
+					System.exit(1);
+					
+				}
+				StdDraw.pause(5);
+			}
+			
 			StdDraw.show();
-			if (choix == 0 && Affichage_ok == 0){
-				Plateau_1.Afficher_map(map,Joueur1,Joueur2);
-				
-				StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
-				StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
-				StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
-				StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
-				
-				//Timer
-				if (Time_seconde < 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde < 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				
-				StdDraw.picture(500, 300, "Menu.png");
-				StdDraw.picture(500, 242, "Survol Resume.png");
-				Affichage_ok = 1;
-				StdDraw.pause(80);
-			}
-			
-			if (choix == 1 && Affichage_ok == 0){
-				Plateau_1.Afficher_map(map,Joueur1,Joueur2);
-				
-				StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
-				StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
-				StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
-				StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
-				
-				//Timer
-				if (Time_seconde < 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde < 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				
-				StdDraw.picture(500, 300, "Menu.png");
-				StdDraw.picture(500, 299, "Survol Restart.png");
-				Affichage_ok = 1;
-				StdDraw.pause(80);
-			}
-			
-			if (choix == 2 && Affichage_ok == 0){
-				Plateau_1.Afficher_map(map,Joueur1,Joueur2);
-				
-				StdDraw.text(1213, 249, Integer.toString((Joueur1.getVie())));
-				StdDraw.text(1213, 454, Integer.toString((Joueur2.getVie())));
-				StdDraw.text(1125, 249, Integer.toString((Joueur1.getNbBombe())));
-				StdDraw.text(1125, 454, Integer.toString((Joueur2.getNbBombe())));
-				
-				//Timer
-				if (Time_seconde < 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute < 10){
-					StdDraw.text(1150, 116,"0" + Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde < 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : 0" + Long.toString(Time_seconde));
-				}
-				else if(Time_seconde >= 10 && Time_minute >= 10){
-					StdDraw.text(1150, 116,Long.toString(Time_minute) + " : " + Long.toString(Time_seconde));
-				}
-				
-				StdDraw.picture(500, 300, "Menu.png");
-				StdDraw.picture(500, 397, "Survol Quit.png");
-				Affichage_ok = 1;
-				StdDraw.pause(80);
-			}
-			
-			if(StdDraw.isKeyPressed(40) && choix < 2){
-				choix = choix + 1;
-				Affichage_ok = 0;
-				
-			}
-			
-			if(StdDraw.isKeyPressed(38) && choix >= 1){
-				choix = choix - 1;
-				Affichage_ok = 0;
-				
-			}
-			
-			//RESUME PRESS
-			if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 0){
-				boucle = false;
-				
-			}
-			
-			//RESTART PRESS
-			if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 1){
-				
-			}
-			
-			//QUIT PRESS
-			if(StdDraw.isKeyPressed(KeyEvent.VK_ENTER) && choix == 2){
-				System.exit(1);
-				
-			}
-			
 		}
 		
 	}
